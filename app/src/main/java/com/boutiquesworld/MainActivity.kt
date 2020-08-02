c1f09ae3a4ffdef2ee20f32838d411c3b786929b
AxelBlaz3 <karthikgaddam4@gmail.com>
2020-08-02 23:01:43 +0530

    Fix crashes due to colors in BoutiquesWorld theme
    Also includes:
    • Add LoginFragment to nav_graph.xml
    • Make LoginFragment as startDestination (temporary).
    • Colors:
      - colorBackground
      - colorOnPrimary
      - colorOnSecondary
      - navigationBarColor
      - statusBarColor
    • Change the height, width dimensions of NavHostFragment.
    • Change mobile and password EditText textAppearance.
    
    Signed-off-by: AxelBlaz3 <karthikgaddam4@gmail.com>

--- app/src/main/java/com/boutiquesworld/MainActivity.kt
+++ app/src/main/java/com/boutiquesworld/MainActivity.kt
@@ -2,16 +2,28 @@ package com.boutiquesworld
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 import com.boutiquesworld.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        findNavController(R.id.nav_host_fragment).addOnDestinationChangedListener(this)
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
     }
 }
