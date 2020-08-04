1efc0f1ee473eb24adb48adaf48fc9943b78e058
AxelBlaz3 <karthikgaddam4@gmail.com>
2020-08-04 20:21:18 +0530

    Show or hide BottomAppBar and FAB based on destination
    Also includes:
    • Treat toolbar as supportActionBar
    
    Signed-off-by: AxelBlaz3 <karthikgaddam4@gmail.com>

--- app/src/main/java/com/boutiquesworld/MainActivity.kt
+++ app/src/main/java/com/boutiquesworld/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.boutiquesworld
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
@@ -12,19 +17,54 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener,
+    View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     @Inject
     lateinit var sessionManager: SessionManager
+
+    private var menuRes = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
         verifyStartDestination()
         findNavController(R.id.nav_host_fragment).addOnDestinationChangedListener(this)
+
+        binding.apply {
+            home.setOnClickListener(this@MainActivity)
+            fabrics.setOnClickListener(this@MainActivity)
+            pending.setOnClickListener(this@MainActivity)
+            profile.setOnClickListener(this@MainActivity)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        binding.toolbar.menu.clear()
+        if (menuRes != -1)
+            menuInflater.inflate(menuRes, menu)
+        return true
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.home -> {
+                Log.d(this.javaClass.simpleName, "Home")
+            }
+            R.id.fabrics -> {
+                Log.d(this.javaClass.simpleName, "Fabrics")
+            }
+            R.id.pending -> {
+                Log.d(this.javaClass.simpleName, "Pending")
+            }
+            else -> {
+                Log.d(this.javaClass.simpleName, "Profile")
+            }
+        }
     }
 
     override fun onDestinationChanged(
@@ -34,11 +74,31 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
     ) {
         when (destination.id) {
             R.id.loginFragment -> {
-                binding.hideAppBar = true
+                supportActionBar?.hide()
+                menuRes = -1
+                onCreateOptionsMenu(binding.toolbar.menu)
+                showHideFabAndBottomAppBar(hideFab = true, hideBottomAppBar = true)
             }
             R.id.productsFragment -> {
-                binding.hideAppBar = false
+                supportActionBar?.show()
+                menuRes = R.menu.main_menu
+                onCreateOptionsMenu(binding.toolbar.menu)
+                showHideFabAndBottomAppBar(hideFab = false, hideBottomAppBar = false)
             }
+            R.id.dashboardFragment -> {
+                supportActionBar?.show()
+                menuRes = R.menu.main_menu
+                onCreateOptionsMenu(binding.toolbar.menu)
+                showHideFabAndBottomAppBar(hideFab = false, hideBottomAppBar = false)
+            }
+            R.id.profileFragment -> {
+                supportActionBar?.show()
+                menuRes = R.menu.main_menu
+                onCreateOptionsMenu(binding.toolbar.menu)
+                showHideFabAndBottomAppBar(hideFab = false, hideBottomAppBar = false)
+            }
+            else -> {
+            } // TODO: Handle unknown navigation
         }
     }
 
@@ -60,5 +120,42 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
                 R.id.loginFragment
 
         findNavController(R.id.nav_host_fragment).graph = navGraph
+    }
+
+    /**
+     * Show or hide FAB and BottomAppBar
+     * @param hideFab: Hides FAB if true, shows it otherwise.
+     * @param hideBottomAppBar: Hides BottomAppBar if true, shows it otherwise.
+     */
+    private fun showHideFabAndBottomAppBar(hideFab: Boolean, hideBottomAppBar: Boolean) {
+        if (hideFab)
+            binding.fab.hide()
+        else
+            binding.fab.show()
+
+        binding.apply {
+            if (!hideBottomAppBar)
+                bottomAppBar.apply {
+                    visibility = View.VISIBLE
+                    performShow()
+                }
+            else {
+                bottomAppBar.performHide()
+                var isCancelled = false
+                bottomAppBar.animate().setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationCancel(animation: Animator?) {
+                        isCancelled = true
+                    }
+
+                    override fun onAnimationEnd(animation: Animator?) {
+                        if (isCancelled)
+                            return
+
+                        bottomAppBar.visibility = View.GONE
+                        fab.visibility = View.INVISIBLE
+                    }
+                })
+            }
+        }
     }
 }
