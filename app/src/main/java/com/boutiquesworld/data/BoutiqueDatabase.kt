793e4841525f28f17e7816ac6ceae91440d3fff0
AxelBlaz3 <karthikgaddam4@gmail.com>
2020-08-04 19:39:34 +0530

    Don't export schema for BoutiqueDatabase
    Signed-off-by: AxelBlaz3 <karthikgaddam4@gmail.com>

--- app/src/main/java/com/boutiquesworld/data/BoutiqueDatabase.kt
+++ app/src/main/java/com/boutiquesworld/data/BoutiqueDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.boutiquesworld.model.Product
 import com.boutiquesworld.model.Retailer
 
-@Database(entities = [Product::class, Retailer::class], version = 1)
+@Database(entities = [Product::class, Retailer::class], version = 1, exportSchema = false)
 abstract class BoutiqueDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDao
     abstract fun retailerDao(): RetailerDao
